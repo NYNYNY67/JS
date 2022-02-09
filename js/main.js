c6b8c45e1574266872a70a0717f04cd61ed08a44
yuhi <yuhi.nagatsuma1996@gmail.com>
2022-02-09 12:59:17 +0900

    DOM

--- js/main.js
+++ js/main.js
@@ -1,12 +1,55 @@
 'use strct';
 
 {
-  const x = 100;
-  const sum = (a, b, c) => a + b + c;
-  console.log(sum(1,2,3));
+
+  class Post {
+    constructor(text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
+
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`)
+    }
+
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+
+    // 静的メソッド
+    static showInfo() {
+      console.log('Post class version 1.0');
+    }
+  }
+
+
+  class SponsoredPost extends Post {
+    constructor(text, sponser) {
+      super(text);
+      this.sponser = sponser;
+    }
+
+    show() {
+      super.show();
+      console.log(`... sponsered by ${this.sponser}`);
+    }
+  }
+
+
+
+  const posts = [
+    new Post("Javascriptの勉強中"),
+    new Post("プログラミング楽しい"),
+    new SponsoredPost('３分動画でマスターしよう', 'dotinstall'),
+  ]
+
+  Post.showInfo();
+
+  posts[2].like();
+
+  // posts[0].show();
+  // posts[1].show();
+
 }
-
-
-const sum = (a, b, c) => a + b + c;
-console.log(sum(1,2,3));
 
